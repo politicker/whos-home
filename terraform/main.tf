1f834e0e89e0d4d229e8c61af54e3baa49bd9c9c
Harrison Borges <hborges005@gmail.com>
2021-12-25 15:53:06 -0500

    try not doing json

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ data "aws_iam_policy" "lambda_sns_publisher_policy" {
 
 resource "aws_iam_role" "whos_home_lambda" {
 	name = "whos_home_lambda"
-	assume_role_policy = data.aws_iam_policy.lambda_sns_publisher_policy.policy.json
+	assume_role_policy = data.aws_iam_policy.lambda_sns_publisher_policy.policy
 }
 
 # Error: handler and runtime must be set when PackageType is Zip
