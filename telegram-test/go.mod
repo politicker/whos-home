f139530abe74a1f541a8e06e68996af5514a6dfc
Quinn Shanahan <q.shanahan@gmail.com>
2021-12-26 13:44:22 -0500

    test role, add access to queue

---
+++ telegram-test/go.mod
@@ -0,0 +1,5 @@
+module github.com/quinn/whos-home/telegram-test
+
+go 1.17
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
--- terraform/role.tf
+++ terraform/role.tf
@@ -39,3 +39,27 @@ resource "aws_iam_role_policy_attachment" "attach_AWSLambdaBasicExecutionRole" {
   role       = aws_iam_role.whos_home_lambda.name
   policy_arn = data.aws_iam_policy.AWSLambdaBasicExecutionRole.arn
 }
+
+data "aws_iam_policy_document" "sqs_access" {
+  statement {
+    sid = "some_id"
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes"
+    ]
+    resources = [
+      aws_sqs_queue.whos_home_queue_telegram_bot.arn
+    ]
+  }
+}
+
+resource "aws_iam_policy" "sqs_access" {
+  name   = "whos_home_lambda_sqs_access"
+  policy = data.aws_iam_policy_document.YOUR_DOCUMENT.json
+}
+
+resource "aws_iam_role_policy_attachment" "sqs_access" {
+  role       = aws_iam_role.whos_home_lambda.name
+  policy_arn = aws_iam_policy.sqs_access.arn
+}
