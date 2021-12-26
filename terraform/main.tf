550e2970ebc6c16cd707b4b5961d241da4b4946f
Quinn Shanahan <q.shanahan@gmail.com>
2021-12-25 19:49:08 -0500

    add output

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,5 +12,5 @@ terraform {
 output "base_url" {
   description = "Base URL for API Gateway stage."
 
-  value = aws_apigatewayv2_stage.whos_home_gateway.invoke_url
+  value = aws_apigatewayv2_stage.whos_home_gateway_stage.invoke_url
 }
