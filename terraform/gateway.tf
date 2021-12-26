27504bc69c58da0d43fbf3a63557ff03a8646486
Quinn Shanahan <q.shanahan@gmail.com>
2021-12-25 19:45:12 -0500

    fix

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -41,7 +41,7 @@ resource "aws_apigatewayv2_integration" "gateway_lambda_integration" {
 }
 
 resource "aws_apigatewayv2_route" "gateway_route" {
-  api_id = aws_apigatewayv2_api.lambda.id
+  api_id = aws_apigatewayv2_api.whos_home_gateway.id
 
   route_key = "GET /hello"
   target    = "integrations/${aws_apigatewayv2_integration.gateway_lambda_integration.id}"
